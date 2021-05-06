fd700250763e891a885809a797a9f8202ac029bf
Brett Fazio <brett.fazio@hotmail.com>
2021-05-05 23:58:34 -0400

    remove block comments

--- source/src/engine/test.rs
+++ source/src/engine/test.rs
@@ -5,13 +5,6 @@ mod engine_tests {
     use crate::types::types::{Order, OrderId, Execution};
     use crate::engine::engine::Engine;
 
-    /*
-      orderid = 0;
-  totaltests++;
-  exec_overflow = 0;
-  execs_out_iter = execs_out;
-  execs_out_len = 0;
-    */
     struct TestState {
         order_id: OrderId,
         engine: Engine,
@@ -46,21 +39,6 @@ mod engine_tests {
         }
 
     }
-
-    /*
-    int test(t_order orders[], unsigned orders_len, t_execution execs[], unsigned execs_len)
-{
-  int ok = 1;
-  set_globals();
-  init();
-  ok = ok && feed_orders(orders, orders_len);
-  ok = ok && assert_exec_count(execs_len);
-  ok = ok && assert_execs(execs, execs_len);
-  destroy();
-  if (!ok)
-    printf("test %i failed.\n\n", totaltests);
-  return ok;
-}*/
 
     fn test(mut orders: Vec<Order>, execs: Vec<Execution>) {
         let mut state = TestState::new();
--- source/src/types/mod.rs
+++ source/src/types/mod.rs
@@ -1,22 +1,3 @@
-/*
-t_orderid
-    Order identification numbers which are returned by limit(). Used to uniquely identify the order for the sake of cancellation.
-
-t_price
-    Price of a share. In the range 0-65536, and interpreted as divided by 100. Eg the range is 000.00-655.36. Eg the price 123.45 = 12345; the price 23.45 = 2345; the price 23.4 = 2340
-
-t_size
-    Number of shares. 
-
-t_side
-    Boolean representing whether a bid (0) or ask (1).
-
-t_order
-    Order submitted to the matching engine containing symbol, trader name, side, price, and size.
-
-t_execution
-    Execution report sent to trader informing them of the symbol, side, price, and size of the transaction.
-*/
 pub mod types {
     pub type OrderId = u64;
 
