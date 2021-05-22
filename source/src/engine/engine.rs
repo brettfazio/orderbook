7e4968d724c18b43c62907b648ba4c7c262a6152
Brett Fazio <brett.fazio@hotmail.com>
2021-05-22 10:18:46 -0400

    revert back to baseline engine in project code, winning still available

--- source/src/engine/engine.rs
+++ source/src/engine/engine.rs
@@ -1,10 +1,8 @@
 /*
-    Reimplementation of the winning QuantCup 1 implementation
-    https://gist.github.com/druska/d6ce3f2bac74db08ee9007cdf98106ef
+    Most basic implementation of matching engine.
 */
 
 use std::vec::Vec;
-use std::collections::{VecDeque, HashMap};
 use core::cmp::min;
 use crate::types::{Order, Price, OrderId, Execution, is_ask};
 
@@ -13,15 +11,9 @@ pub struct OrderIn {
     id: OrderId,
 }
 
-struct PricePoint {
-    items: VecDeque<OrderId>
-}
-
 pub struct Engine {
-    ask_min: Price,
-    bid_max: Price,
-    book_entries: HashMap<OrderId, OrderIn>,
-    price_points: Vec<PricePoint>,
+    bids: Vec<OrderIn>,
+    asks: Vec<OrderIn>,
     id: OrderId,
     pub execution_log: Vec<Execution>,
     should_log: bool
@@ -29,34 +21,42 @@ pub struct Engine {
 
 impl Engine {
 
-    fn _new(debug: bool) -> Engine {
-        let max_orders = 1010000;
-
-        let mut pps: Vec<PricePoint> = Vec::with_capacity((Price::max_value() as usize) + 1);
-
-        let mut idx = 0;
-        while idx < (Price::max_value() as usize) + 1 {
-            pps.push(PricePoint{ items: VecDeque::new() });
-            idx += 1;
-        }
-
+    pub fn new() -> Engine {
         Engine {
-            ask_min: 0,
-            bid_max: 0,
-            book_entries: HashMap::new(),
-            price_points: pps,
+            bids: Vec::<OrderIn>::new(),
+            asks: Vec::<OrderIn>::new(),
             id: 1,
             execution_log: Vec::new(),
-            should_log: debug
+            should_log: false
         }
-    }
-
-    pub fn new() -> Engine {
-        Engine::_new(false)
     }
 
     pub fn new_debug() -> Engine {
-        Engine::_new(true)
+        Engine {
+            bids: Vec::<OrderIn>::new(),
+            asks: Vec::<OrderIn>::new(),
+            id: 1,
+            execution_log: Vec::new(),
+            should_log: true
+        }
+    }
+    
+    // Helpers for cross
+    fn hit_ask(bid: Price, ask: Price) -> bool {
+        return bid >= ask;
+    }
+
+    fn hit_bid(ask: Price, bid: Price) -> bool {
+        return ask <= bid;
+    }
+
+    // Helpers for queue
+    fn priority_ask(ask_new: Price, ask_old: Price) -> bool {
+        return ask_new < ask_old;
+    }
+
+    fn priority_bid(bid_new: Price, bid_old: Price) -> bool {
+        return bid_new > bid_old;
     }
 
     // Original implementation used an undefined header function in the engine.h 
@@ -78,9 +78,6 @@ impl Engine {
     }
 
     fn trade(order: &mut Order, matched_order: &mut Order, log: &mut Vec<Execution>, should_log: bool) {
-        if matched_order.size == 0 {
-            return;
-        }
         if should_log {
             // Send to execution report now.
             Engine::send_execution(&order.clone(), &matched_order.clone(), log);
@@ -100,135 +97,56 @@ impl Engine {
         }
     }
 
-    fn queue(&mut self, order: Order) -> OrderId {
-        // Add to price point.
-        self.price_points[order.price as usize].items.push_back(self.id);
-        // Add to book entries.
-        self.book_entries.insert(self.id, OrderIn { order: order, id: self.id });
+    fn cross(&mut self, order: &mut Order) -> bool {
+        let isask = is_ask(order.side);
+        let book = if isask { &mut self.bids } else { &mut self.asks };
+        let cross_test = if isask { Engine::hit_bid } else { Engine::hit_ask };
+        let log = &mut self.execution_log;
 
-        // Return new order number
+        for matched_order in book.iter_mut() {
+            if order.size == 0 {
+                break;
+            }
+            if !cross_test(order.price, matched_order.order.price) {
+                break;
+            }
+
+            Engine::trade(order, &mut matched_order.order, log, self.should_log);            
+        }
+
+        book.retain(|x| x.order.size > 0);
+
+        order.size == 0
+    }
+
+    fn queue(&mut self, order: Order) {
+        let isask = is_ask(order.side);
+        let book = if isask { &mut self.asks } else { &mut self.bids };
+        let cross_test = if isask { Engine::priority_ask } else { Engine::priority_bid };
+
+        let insertion_index = match book.iter().enumerate().find(|(_index, ele)| cross_test(order.price, ele.order.price)) {
+            Some((a, _)) => a,
+            _ => book.len(),
+        };
+                            
+        let new_order = OrderIn { order: order, id: self.id };
+        book.insert(insertion_index, new_order);
+    }
+
+    pub fn limit_order(&mut self, mut order: Order) -> OrderId {
+        // Cross off as many shares as possible.
+        if !self.cross(&mut order) {
+            // Queue order if all shares not crossed off.
+            self.queue(order);
+        }
         let return_id = self.id;
         self.id += 1;
         return_id
     }
 
-    pub fn limit_order(&mut self, mut order: Order) -> OrderId {
-        // Cross off as many shares as possible.
-        if is_ask(order.side) {
-            if order.price >= self.ask_min {
-                let mut pp_entry = &mut self.price_points[self.ask_min as usize];
-
-                loop {
-                    let mut entries = &mut pp_entry.items;
-
-                    // Go over entries
-                    for item_id in entries.iter_mut() {
-                        let mut item = &mut self.book_entries.get_mut(&*item_id).unwrap().order;
-                        Engine::trade(&mut order, &mut item, &mut self.execution_log, self.should_log);
-
-                        if order.size == 0 {
-                            break;
-                        }
-                    }
-                    // Remove
-                    loop {
-                        match entries.front() {
-                            Some(x) => {
-                                if self.book_entries.get(&*x).unwrap().order.size == 0 {
-                                    entries.pop_front();
-                                }else {
-                                    break;
-                                }
-                            }
-                            None => break
-                        }
-                    }
-                    
-                    if order.size == 0 {
-                        let return_id = self.id;
-                        self.id += 1;
-                        return return_id;
-                    }
-
-                    // All orders at the current price point.
-                    self.ask_min += 1;
-                    if order.price < self.ask_min {
-                        break;
-                    }
-                    pp_entry = &mut self.price_points[self.ask_min as usize];
-                }
-            }
-
-            // Adjust potential max
-            if self.bid_max < order.price {
-                self.bid_max = order.price;
-            }
-            // Queue order
-            let new_id = self.queue(order);
-            return new_id;
-        }
-        else { // sell
-            if order.price <= self.bid_max {
-                let mut pp_entry = &mut self.price_points[self.bid_max as usize];
-
-                loop {
-                    let mut entries = &mut pp_entry.items;
-
-                    // Go over entries
-                    for item_id in entries.iter_mut() {
-                        let mut item = &mut self.book_entries.get_mut(&*item_id).unwrap().order;
-                        Engine::trade(&mut order, &mut item, &mut self.execution_log, self.should_log);
-
-                        if order.size == 0 {
-                            break;
-                        }
-                    }
-                    // Remove
-                    loop {
-                        match entries.front() {
-                            Some(x) => {
-                                if self.book_entries.get(&*x).unwrap().order.size == 0 {
-                                    entries.pop_front();
-                                }else {
-                                    break;
-                                }
-                            }
-                            None => break
-                        }
-                    }
-                    
-                    if order.size == 0 {
-                        let return_id = self.id;
-                        self.id += 1;
-                        return return_id;
-                    }
-
-                    // All orders at the current price point.
-                    self.bid_max -= 1;
-                    if order.price > self.bid_max {
-                        break;
-                    }
-                    pp_entry = &mut self.price_points[self.ask_min as usize];
-                }
-
-            }
-
-            // Adjust potential max
-            if self.ask_min > order.price {
-                self.ask_min = order.price;
-            }
-            // Queue order
-            let new_id = self.queue(order);
-            // Return new order number
-            return new_id;
-        }
-    }
-
     pub fn cancel(&mut self, id: OrderId) {
-        match self.book_entries.get_mut(&id) {
-            Some(x) => x.order.size = 0,
-            None => return,
-        }
+        self.asks.retain(|x| x.id != id);
+        self.bids.retain(|x| x.id != id);
     }
 
 }
